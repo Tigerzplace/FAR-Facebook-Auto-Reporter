1c875afef28f65ba5a270554fa3b02e880eefdc2
Tigerzplace <47848999+Tigerzplace@users.noreply.github.com>
2023-04-07 19:25:00 +0500

    1.2

--- js/background.js
+++ js/background.js
@@ -1,27 +1,10 @@
-/*
-	Facebook Auto Reporter
- coded by Nasir Ali (fb.com/tiger6117)
-video tutorials can be found @ : https://goo.gl/8Xedwf
-get more social sites tweaky stuff updates @ : facebook.com/tigerzplace
-for more scripts and softwares visit : https://www.tigerzplace.com
-
-
-Donate ( Feel Free to help me out with my work ) : 
-✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦
-
-BTC: 1LrcMaefjnCz4nf8ViKR6EwHqySPAvCCEY
-DASH: XpVNfgAnYvLU79k5xuKc4r24md936ckPHu
-ETH: 0x56c9a1BC8E1BF2dD83ba512A3F9C22eff33beEF3
-BNK : 0x56c9a1BC8E1BF2dD83ba512A3F9C22eff33beEF3
-LTC:  Levu8eJ6j9fsJe5tX8esGecLzCjvEirEC2
-BCH : qqva7j6c353vkvcfky99ccxz5jjsd50mzv8dvyzyhu
------------------------------------------------
-If you want to receive XEM to your account, you MUST provide sender with your Address and the unique Message below
-
-XEM:NBRA5SR2S47YFIXPQAFU2FZ5BYFDMJELO3XWLV2Q
-MESSAGE:yadB37sQ
-------------------------------------------------
-
-✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦
-
-*/var _0xa143=["\x23\x62\x62\x31\x35\x31\x31","\x73\x65\x74\x42\x61\x64\x67\x65\x42\x61\x63\x6B\x67\x72\x6F\x75\x6E\x64\x43\x6F\x6C\x6F\x72","\x62\x72\x6F\x77\x73\x65\x72\x41\x63\x74\x69\x6F\x6E","\x58","\x73\x65\x74\x42\x61\x64\x67\x65\x54\x65\x78\x74","","\x61\x64\x64\x4C\x69\x73\x74\x65\x6E\x65\x72","\x6F\x6E\x44\x69\x73\x63\x6F\x6E\x6E\x65\x63\x74","\x6F\x6E\x43\x6F\x6E\x6E\x65\x63\x74","\x72\x75\x6E\x74\x69\x6D\x65"];chrome[_0xa143[9]][_0xa143[8]][_0xa143[6]](function(_0xc5b6x1){_0xc5b6x1[_0xa143[7]][_0xa143[6]](function(){chrome[_0xa143[2]][_0xa143[1]]({color:_0xa143[0]});chrome[_0xa143[2]][_0xa143[4]]({text:_0xa143[3]});setTimeout(function(){chrome[_0xa143[2]][_0xa143[4]]({text:_0xa143[5]})},3000)})})
+chrome.runtime.onConnect.addListener(function (externalPort)  {
+	externalPort.onDisconnect.addListener(function() {
+							chrome.browserAction.setBadgeBackgroundColor({color: "#bb1511"});
+							chrome.browserAction.setBadgeText({text: "X"});
+							
+							setTimeout (function (){
+								chrome.browserAction.setBadgeText({text: ""});
+							},3000);
+	});
+});
