12b77266461a124f9fa7849f4fed52905b9e6593
Tigerzplace <47848999+Tigerzplace@users.noreply.github.com>
2023-04-13 04:11:11 +0500

    2.5.2
    v2.5.2
    
    > Not completing report fixed.
    > Logout issue fixed, after reporting from one account.
    > All report links and complete report module is optimized.
    > New report links for pages included, more links will be added soon.
    > Checking url (valid or not), is fixed and will not stop reporter without any notification.
    > Skipping account error fixed (will only show wrong login details after double checking the account)

--- js/background.js
+++ js/background.js
@@ -1,15 +1,68 @@
-chrome.runtime.onConnect.addListener(function (externalPort)  {
-	externalPort.onDisconnect.addListener(function() {
-							chrome.browserAction.setBadgeBackgroundColor({color: "#bb1511"});
-							chrome.browserAction.setBadgeText({text: "X"});
-							
-							setTimeout (function (){
-								chrome.browserAction.setBadgeText({text: ""});
-							},3000);
+chrome.runtime.onConnect.addListener(function (externalPort) {
+	externalPort.onDisconnect.addListener(function () {
+		chrome.browserAction.setBadgeBackgroundColor({ color: "#bb1511" });
+		chrome.browserAction.setBadgeText({ text: "X" });
+
+		setTimeout(function () {
+			chrome.browserAction.setBadgeText({ text: "" });
+		}, 3000);
 	});
-}); 
+});
+
+
+
+function injectContentScript(tabId) {
+	chrome.tabs.executeScript(
+	  tabId,
+	  { file: '/js/newContent.js' },
+	  function() {
+		console.log('Content script loaded');
+	  }
+	);
+  }
+
+
 
 /*
+
+
+function injectContentScript(tabId) {
+	chrome.tabs.executeScript(
+		tabId,
+		{
+			file: '/js/newContent.js',
+			allFrames: true // <-- execute in all frames of the tab
+		},
+		function (results) {
+			if (chrome.runtime.lastError) {
+				console.error(chrome.runtime.lastError);
+			} else {
+				// Check if the content script has already been loaded
+				var isLoaded = false;
+				for (var i = 0; i < results.length; i++) {
+					if (results[i] !== null) {
+						isLoaded = true;
+						break;
+					}
+				}
+				if (!isLoaded) {
+					// Inject the content script
+					chrome.tabs.executeScript(
+						tabId,
+						{ file: '/js/newContent.js' },
+						function () {
+							console.log('Content script loaded');
+						}
+					);
+				} else {
+					console.log('Content script already loaded');
+				}
+			}
+		}
+	);
+}
+
+
 function clearLogs() {
 	chrome.storage.local.remove("logs", function() {
 	  console.log("Logs cleared");
